9bfeafc1fab1873bb2bb785a110e82d8afdf4c0b
ItzDenkiVN <denkilomao@gmail.com>
2022-05-07 18:09:38 +0700

    API

--- server.js
+++ server.js
@@ -17,42 +17,19 @@ const apikey = [
     "DjisTNQfWctm",
     "x3GlnWAp9p9U",
     "XINe14HQ267h",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    ""
+    "DCaQ1P3aYoh1",
+    "surjhD1Ph94v",
+    "I0fYjVDyybaJ",
+    "Lwy4bCgNnWkl",
+    "mo6eYhgRen77",
+    "xXHhdOc1Mmgq",
+    "ToQ2XgHVXPNq",
+    "nG4xq79QhA2x",
+    "f3copk6G6tjp",
+    "cyKUMBDzTX17",
+    "i3wluRxpIkCq",
+    "VfqyrGoN6znc",
+    "Rf6x8bckLLvg"
 ]
 app.get("/", (req, res) => {
     res.send("https://github.com/ItzDenkiRepo/VtuberAPI")
@@ -77,7 +54,26 @@ app.get("/randomvtuber", (req, res) => {
         res.send(JSON.stringify(result, null, 2))
     }
 })
-
+app.get("/cosplay", (req, res) => {
+    const key = req.query.apikey
+    const result = {}
+    result.code = 200
+    const imageList = fs.readdirSync("./public/cosplay")
+    const randomImage = imageList[Math.floor(Math.random() * imageList.length)]
+    result.url = `127.0.0.1:5000/cosplay/${randomImage}`
+    result.author = "ItzDenki , VtuberAPI"
+    result.source = "https://github.com/ItzDenkiRepo/VtuberAPI"
+    res.header("Content-type", "application/json; charset=utf-8")
+    if (apikey.includes(key)) {
+        res.send(JSON.stringify(result, null, 2))
+        console.log(result)
+    } else {
+        const result = {}
+        result.code = 403
+        result.message = "API Key không phù hợp"
+        res.send(JSON.stringify(result, null, 2))
+    }
+})
 app.listen(port, "0.0.0.0", function () {
     console.log(`Server listening on port ${port}\n`)
 })
